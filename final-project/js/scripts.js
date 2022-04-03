f2fed8bf8c650f8a0fc16e153c36be64f39a90de
Abdalmohsen16 <100500608+Abdalmohsen16@users.noreply.github.com>
2022-04-03 23:07:03 +0300

    done

---
+++ final-project/js/scripts.js
@@ -0,0 +1,17 @@
+// let selection = document.querySelector('select')
+// let link = document.getElementById('submit');
+
+// selection.addEventListener('change', () => {
+//     link.innerHTML = `` ;
+// })
+
+let link1 = "تـأكـيـد"
+
+function Medicine() {
+    document.getElementById('submit').innerHTML = `
+    <a href="https://www.w3schools.com/howto/howto_js_add_class.asp"
+    style="color: white">
+    ${link1}
+    </a>
+    `
+}
